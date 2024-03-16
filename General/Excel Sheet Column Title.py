8ab7bb2e1bc691ad8ca41df205706cb547d09215
Ahmad Awwad <aawwad172@gmail.com>
2024-03-16 05:09:26 +0300

    Solving the Excel Sheet Column Title

---
+++ General/Excel Sheet Column Title.py
@@ -0,0 +1,39 @@
+class Solution:
+    def titleToNumber(self, columnTitle: str) -> int:
+        letters = {
+            "A": 1,
+            "B": 2,
+            "C": 3,
+            "D": 4,
+            "E": 5,
+            "F": 6,
+            "G": 7,
+            "H": 8,
+            "I": 9,
+            "J": 10,
+            "K": 11,
+            "L": 12,
+            "M": 13,
+            "N": 14,
+            "O": 15,
+            "P": 16,
+            "Q": 17,
+            "R": 18,
+            "S": 19,
+            "T": 20,
+            "U": 21,
+            "V": 22,
+            "W": 23,
+            "X": 24,
+            "Y": 25,
+            "Z": 26,
+        }
+
+        result = 0
+        if len(columnTitle) == 1:
+            result += letters[columnTitle]
+            return result
+        else:
+            for i in range(len(columnTitle)):
+                result = result * 26 + letters[columnTitle[i]]
+            return result
